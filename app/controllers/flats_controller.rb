b8caeb07ad4388b0a33a63782199eb22e6019cd1
Claudine McGowan <claudine.mcgowan@gmail.com>
2020-10-12 21:13:15 +0100

    pages working

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -11,12 +11,33 @@ before_action :find_flat, only: [:show, :edit, :create, :destroy]
   def show; end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
